b4e8f467fa9c9c65a53ee424cd48d3d769423f0a
Nicolas A. Dela Cruz <ndelacruz@cascade.local>
2022-06-24 13:35:27 -0700

    thought about random game idea involing just a text field element and creating some sort of art

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
 
 
 
 
+window.onload=function(){
+    document.getElementById('new-item-input').addEventListener('input', function() {
+        console.log(this.value);
+    });
+  }
 
-console.log("end of script")
+// console.log(document.getElementById('new-item-input').value);
