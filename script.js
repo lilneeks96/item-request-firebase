63b7adfc7ab13efee231fbb3ad304838abc3ba7e
Nicolas A. Dela Cruz <ndelacruz@cascade.local>
2022-06-24 14:13:51 -0700

    I did a thing a good thing logical

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 
+
+
 var items = [];
 
 function pushArray() {
@@ -10,3 +12,21 @@ function pushArray() {
     console.log(items);
     return false;
   }
+
+
+
+const btn = document.getElementById('hidden-submit-btn');
+
+btn.addEventListener('click', function handleClick(event) {
+  // 👇️ if you are submitting a form (prevents page reload)
+  event.preventDefault();
+
+  const firstNameInput = document.getElementById('new-item-input');
+
+  // Send value to server
+  console.log(firstNameInput.value);
+  pushArray() 
+
+  // 👇️ clear input field
+  firstNameInput.value = '';
+});
